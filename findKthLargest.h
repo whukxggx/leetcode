c525216a3e54eeaf98c9065c9cfcfacd2a2e5143
whukxggx <2534423312@qq.com>
2020-06-21 23:26:40 +0800

    选择第k大数

--- findKthLargest.h
+++ findKthLargest.h
@@ -1,27 +1,30 @@
+//215 数组中第k大元素
+#include"head.h"
+
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         int size=nums.size();
-        return quickSelect(nums,0,size-1,size-1-k);
-    }
+        return quickSelect(nums,0,size-1,size-k);
+    }   
     int quickSelect(vector<int> &nums,int begin,int end,int k_small){
         if(begin==end)return nums[begin];
-        int p=begin+paration(nums,begin,end);
+        int p=paration(nums,begin,end);
         if(p==k_small)return nums[p];
         else if(p>k_small) return quickSelect(nums,begin,p-1,k_small);
         return quickSelect(nums,p+1,end,k_small);
-    }
+    }   
     int paration(vector<int> &nums,int begin,int end){
         int x=nums[begin];
         int i=begin+1;
         for(int j=begin+1;j<=end;++j){
-            if(nums[j]<nums[i]){
+            if(nums[j]<=x){
                 swap(nums[i],nums[j]);
                 ++i;
-            }
-        }
+            }   
+        }   
         --i;
         swap(nums[begin],nums[i]);
         return i;
-    }
+    }   
 }; 
