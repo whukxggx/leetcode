83c3c09f16468222f08fc43381dabc9c30c572c4
whukxggx <2534423312@qq.com>
2021-03-29 09:46:33 +0800

    颠倒二进制

---
+++ reverseBits.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "head.h"
+
+class Solution {
+public:
+	uint32_t reverseBits(uint32_t n) {
+		int res = 0;
+		int i = 32;
+		while (i--) {
+			res <<= 1;
+			res += n & 1;
+			n >>= 1;
+		}
+		return res;
+	}
+};
