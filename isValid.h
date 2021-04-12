f294770f076b9322b9aecde29ee560ae247971cd
whukxggx <2534423312@qq.com>
2021-04-12 10:47:35 +0800

    new

--- isValid.h
+++ isValid.h
@@ -1,44 +1,26 @@
+#pragma once
 #include "head.h"
 
-class Solution
-{
+class Solution {
 public:
-    bool isValid(string s)
-    {
-        int size = s.size();
-        if (size == 0)
-            return true;
-        if (size == 1)
-            return false;
-        stack<char> sta;
-        int i = 0;
-        while (i < size)
-        {
-            switch (s[i])
-            {
-            case '(':
-            case '{':
-            case '[':
-                sta.push(s[i]);
-                break;
-            case ')':
-                if (sta.top() != '(')
-                    return false;
-                sta.pop();
-                break;
-            case '}':
-                if (sta.top() != '{')
-                    return false;
-                sta.pop();
-                break;
-            case ']':
-                if (sta.top() != '[')
-                    return false;
-                sta.pop();
-                break;
-            }
-            ++i;
-        }
-        return sta.empty();
-    }
+	bool isValid(string s) {
+		int size = s.size();
+		if (size % 2 == 1)return false;
+		stack<char> c_stack;
+		unordered_map<char, char> pairs = {
+			{'(',')'},{'[',']'},{'{','}'}
+		};
+		for (char c : s) {
+			if (c=='('||c=='['||c=='{') {
+				c_stack.push(c);
+			}
+			else {
+				if (c_stack.empty() || pairs[c_stack.top()] != c) {
+					return false;
+				}
+				c_stack.pop();
+			}
+		}
+		return c_stack.empty();
+	}
 };
