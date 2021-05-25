7903ea5b5d4fc9e88bde46b168e903a5e7ff640b
whukxggx <2534423312@qq.com>
2021-05-25 10:56:00 +0800

    买卖股票最佳时机

--- maxProfit.h
+++ maxProfit.h
@@ -1,38 +1,21 @@
-#include"head.h"
+#pragma once
+#include "head.h"
 
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int size=prices.size();
-        if(size==0||size==1)return 0;
-        if(size==2)return max(0,prices[1]-prices[0]);
-        vector<int> dp(size,0);
-        dp[0]=0;
-        dp[1]=max(dp[0],prices[1]-prices[0]);
-        int _min=min(prices[0],prices[1]);
-        for(int i=2;i<size;++i){
-            dp[i]=max(dp[i-1],prices[i]-_min);
-            _min=min(_min,prices[i]);
-        }
-        return dp[size-1];
-    }
-};
-
-
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int size=prices.size();
-        if(size==0||size==1)return 0;
-        if(size==2)return max(0,prices[1]-prices[0]);
-        int maxProfit=0;
-        int _min=prices[0];
-        for(int i=1;i<size;++i){
-            if(prices[i]-_min>maxProfit){
-                maxProfit=prices[i]-_min;
-            }
-            _min=min(_min,prices[i]);
-        }
-        return maxProfit;
-    }
+	int maxProfit(vector<int>& prices) {
+		int size = prices.size();
+		int minValue = INT_MAX;
+		int maxRes = INT_MIN;
+		for (int i = 0; i < size; ++i) {
+			if (minValue > prices[i]) {
+				minValue = prices[i];
+			}
+			else if(prices[i]-minValue>maxRes){
+				maxRes = prices[i] - minValue;
+			}
+		}
+		if (maxRes== INT_MIN)return 0;
+		return maxRes;
+	}
 };
---
+++ maxProfit2.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "head.h"
+
+class Solution {
+public:
+	int maxProfit(vector<int>& prices) {
+		int size = prices.size();
+		int ans = 0;
+		for (int i = 1; i < size; ++i) {
+			if (prices[i] > prices[i - 1])
+				ans += (prices[i] - prices[i - 1]);
+		}
+		return ans;
+	}
+};
