c022ca4cc3ea59f75950cb29c887190ebef3e847
whukxggx <2534423312@qq.com>
2021-06-08 09:01:16 +0800

    整数拆分

---
+++ 动态规划/integerBreak.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "head.h"
+
+class Solution {
+public:
+	int integerBreak(int n) {
+		vector<int> dp(n + 1, 0);
+		dp[1] = 1;
+		for (int i = 2; i <= n; ++i) {
+			for (int j = 1; j < i; ++j) {
+				dp[i] = max(dp[i], max(dp[i - j] * j, (i - j)*j));
+			}
+		}
+		return dp[n];
+	}
+};
